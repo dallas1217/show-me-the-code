8b3d8a5acb5c6ee951e3c3f20182beaf32098502
dallas1217 <215861804@qq.com>
2017-06-14 16:39:52 +0800

    Update 0004.py

--- 0004/0004.py
+++ 0004/0004.py
@@ -1,1 +1,14 @@
 
+#!/usr/bin/python
+
+import re
+
+count=0
+
+f=open('words.txt','rb')
+pattern=re.compile(r'[a-zA-Z]+')
+for data in f.readlines():
+    count+=len(pattern.findall(data))
+f.close()
+
+print count
